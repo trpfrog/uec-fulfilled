bf7fc2a80cb86654ea913156c7c22fc42450dc33
TrpFrog <dev@trpfrog.net>
2024-03-01 22:22:14 +0900

    body 直下に table があった場合も読めるように変更

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,53 @@ import { eraseColumns } from "./erase";
 import { writeColumn } from "./write";
 
 function getCreditTable(): HTMLTableElement {
-	const frames = Array.from(document.querySelectorAll("frame"));
-	for (const frame of frames) {
-		const tables = Array.from(
-			frame.contentDocument?.querySelectorAll("table") || [],
-		) as HTMLTableElement[];
+  const tables = [
+    // 現在学務情報システムには frameset の中にテーブルがあるので頑張って持ってくる
+    ...Array.from(document.querySelectorAll("frame")).flatMap((frame) =>
+      Array.from(frame.contentDocument?.querySelectorAll("table") || []),
+    ),
+    // 学務情報システムが正気を取り戻して frameset をやめたとき用
+    ...Array.from(document.querySelectorAll("table") || []),
+  ];
 
-		// tr に 区分, 所要単位, 修得単位 の含まれているテーブルを取得
-		for (const table of tables) {
-			const row = table.querySelector("tr")?.innerHTML;
-			if (["区分", "所要単位", "修得単位"].every((s) => row?.includes(s))) {
-				return table;
-			}
-		}
-	}
-	throw "成績表が見つかりませんでした。";
+  // tr に 区分, 所要単位, 修得単位 の含まれているテーブルを取得
+  for (const table of tables) {
+    const row = table.querySelector("tr")?.innerHTML;
+    if (["区分", "所要単位", "修得単位"].every((s) => row?.includes(s))) {
+      return table;
+    }
+  }
+
+  throw "成績表が見つかりませんでした。";
 }
 
 function isAlreadyAdded(table: HTMLTableElement) {
-	return Array.from(table.querySelectorAll("tr > *")).some(
-		(td) => td.textContent === "残り単位数",
-	);
+  return Array.from(table.querySelectorAll("tr > *")).some(
+    (td) => td.textContent === "残り単位数",
+  );
 }
 
 function main() {
-	const url = new URL(location.href);
-	if (url.hostname !== "campusweb.office.uec.ac.jp") {
-		alert("このブックマークレットは学務情報システム上でのみ使用できます。");
-		return;
-	}
-	try {
-		const creditTable = getCreditTable();
-		if (isAlreadyAdded(creditTable)) {
-			eraseColumns(creditTable);
-		} else {
-			writeColumn(creditTable);
-		}
-	} catch (error) {
-		alert(error);
-		return;
-	}
+  const url = new URL(location.href);
+  if (
+    url.hostname && // in development, url.hostname is ""
+    url.hostname !== "campusweb.office.uec.ac.jp" &&
+    url.hostname !== "trpfrog.github.io"
+  ) {
+    alert("このブックマークレットは学務情報システム上でのみ使用できます。");
+    return;
+  }
+  try {
+    const creditTable = getCreditTable();
+    if (isAlreadyAdded(creditTable)) {
+      eraseColumns(creditTable);
+    } else {
+      writeColumn(creditTable);
+    }
+  } catch (error) {
+    alert(error);
+    return;
+  }
 }
 
 main();
