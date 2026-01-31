10d04a319496fd87d3cd091b99ae30637424f994
tsmami <dev@trpfrog.net>
2026-02-01 01:29:03 +0900

    style: check-write をコードベースに適用

--- src/erase.ts
+++ src/erase.ts
@@ -1,17 +1,17 @@
 export function eraseColumns(creditTable: HTMLTableElement) {
-	// 各行 (tr) が3つの td を持つようにする
-	const rows = Array.from(
-		creditTable.querySelectorAll("tr"),
-	) as HTMLTableRowElement[];
-	for (const row of rows) {
-		row.style.backgroundColor = "";
-		while (row.children.length > 3) {
-			const lastChild = row.lastElementChild;
-			if (lastChild) {
-				row.removeChild(lastChild);
-			} else {
-				break;
-			}
-		}
-	}
+  // 各行 (tr) が3つの td を持つようにする
+  const rows = Array.from(
+    creditTable.querySelectorAll("tr"),
+  ) as HTMLTableRowElement[];
+  for (const row of rows) {
+    row.style.backgroundColor = "";
+    while (row.children.length > 3) {
+      const lastChild = row.lastElementChild;
+      if (lastChild) {
+        row.removeChild(lastChild);
+      } else {
+        break;
+      }
+    }
+  }
 }
